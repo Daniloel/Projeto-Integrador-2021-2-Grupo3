0e0d8c46d0e54a63d67a013a9a25c4357ccaa9ce
LeoAdlerr <88751032+LeoAdlerr@users.noreply.github.com>
2021-11-09 18:09:50 -0300

    Update programasprint3.py

--- programasprint3.py
+++ programasprint3.py
@@ -20,7 +20,8 @@ x = str('')
 
 while x != ("cidade", "estado"):
 
-    x = str(input("\033[0;34mDigite:(cidade) para as cidades, (estado) para estadoSP e (X) para finalizar pesquisa: \033[m")).lower()
+    x = str(input(
+        "\033[0;34mDigite:(cidade) para as cidades, (estado) para estadoSP e (X) para finalizar pesquisa: \033[m")).lower()
 
     if x == "estado":
 
@@ -28,12 +29,18 @@ while x != ("cidade", "estado"):
 
         print('''\033[0;35mEscolha uma das opçoes
                 [ 1 ] data expecifica
-                [ 2 ] última data disponível\033[m''')
+                [ 2 ] última data disponível
+                [ 3 ] Ano de 2020
+                [ 4 ] Ano de 2021
+                [ 5 ] 1ºSemestre 2020
+                [ 6 ] 2ºSemestre 2020
+                [ 7 ] 1ºSemestre 2021
+                [ 8 ] 2ºSemestre 2021\033[m''')
         esc = str("")
 
-        while esc != ("1", "2", "3"):
+        while esc != ("1", "2", "3", "4", "5", "6", "7", "8"):
 
-            esc = str(input('Digite a sua escolha(1 ou 2): '))
+            esc = str(input('Digite a sua escolha(1, 2, 3, 4, 5 ,6, 7, 8): '))
 
             if esc == "1":
                 dt2 = input("\033[0;35mDigite a data nesse formato(ano-mês-dia)ex:yyyy-mm-dd:\033[m")
@@ -45,7 +52,6 @@ while x != ("cidade", "estado"):
                     dt2 = input("Digite a data nesse formato(ano-mês-dia)ex:yyyy-mm-dd:")
                     colDT2 = colSP1.loc[colSP1["date"] == dt2]
 
-                colDT2 = colDT2.drop("date", axis=1)
                 colDT2 = colDT2.drop("state", axis=1)
                 colDT2 = colDT2.drop("place_type", axis=1)
                 colDT2.rename(columns={'city': 'cidade', 'estimated_population': 'população',
@@ -59,7 +65,6 @@ while x != ("cidade", "estado"):
             elif esc == "2":
                 dts2 = colSP1.loc[colSP1["date"] == "2021-05-10"]
 
-                dts2 = dts2.drop("date", axis=1)
                 dts2 = dts2.drop("state", axis=1)
                 dts2 = dts2.drop("place_type", axis=1)
                 dts2.rename(columns={'city': 'cidade', 'estimated_population': 'população',
@@ -68,28 +73,107 @@ while x != ("cidade", "estado"):
                                      'last_available_deaths': 'últimos óbitos',
                                      'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
                 print(dts2)
-            else:
                 break
+            elif esc == "3":
+                Y = colSP1[colSP1["date"].between("2020-01-01", "2020-12-31")]
+                Y = Y.drop("state", axis=1)
+                Y = Y.drop("place_type", axis=1)
+                Y.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(Y)
+                break
+
+            elif esc == "4":
+                y = colSP1[colSP1["date"].between("2021-01-01", "2021-12-31")]
+                y = y.drop("state", axis=1)
+                y = y.drop("place_type", axis=1)
+                y.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(y)
+                break
+
+            elif esc == "5":
+                s = colSP1[colSP1["date"].between("2020-01-01", "2020-06-30")]
+                s = s.drop("state", axis=1)
+                s = s.drop("place_type", axis=1)
+                s.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(s)
+                break
+
+            elif esc == "6":
+                S = colSP1[colSP1["date"].between("2020-07-01", "2020-12-31")]
+                S = S.drop("state", axis=1)
+                S = S.drop("place_type", axis=1)
+                S.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(S)
+                break
+
+            elif esc == "7":
+                s1 = colSP1[colSP1["date"].between("2021-01-01", "2020-06-30")]
+                s1 = s1.drop("state", axis=1)
+                s1 = s1.drop("place_type", axis=1)
+                s1.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                   'last_available_confirmed': 'últimos confirmados',
+                                   'last_available_death_rate': 'taxa de óbitos',
+                                   'last_available_deaths': 'últimos óbitos',
+                                   'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(s1)
+                break
+
+            elif esc == "8":
+                S1 = colSP1[colSP1["date"].between("2021-07-01", "2021-12-31")]
+                S1 = S1.drop("state", axis=1)
+                S1 = S1.drop("place_type", axis=1)
+                S1.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                   'last_available_confirmed': 'últimos confirmados',
+                                   'last_available_death_rate': 'taxa de óbitos',
+                                   'last_available_deaths': 'últimos óbitos',
+                                   'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(S1)
+                break
+
 
     elif x == "cidade":
 
         colSP1 = colSP.loc[colSP["place_type"] == "city"]
 
-        cd = input("\033[0;34mDigite o nome da cidade(escreva com letras maiusculas, minusculas e com acento)ex:São Paulo: \033[m")
+        cd = input(
+            "\033[0;34mDigite o nome da cidade(escreva com letras maiusculas, minusculas e com acento)ex:São Paulo: \033[m")
 
         colCD = colSP1.loc[colSP1["city"] == cd]
         while colCD.empty:
             print("\033[0;31mCidade não encontrada\nDigite o nome da cidade novamente\033[m")
-            cd = input("\033[0;34mDigite o nome da cidade(escreva com letras maiusculas, minusculas e com acento)ex:São Paulo: \033[m")
+            cd = input(
+                "\033[0;34mDigite o nome da cidade(escreva com letras maiusculas, minusculas e com acento)ex:São Paulo: \033[m")
             colCD = colSP1.loc[colSP1["city"] == cd]
 
         print('''\033[0;35mEscolha uma das opçoes
                         [ 1 ] data expecifica
-                        [ 2 ] todas as datas\033[m''')
+                        [ 2 ] última data disponível
+                        [ 3 ] Ano de 2020
+                        [ 4 ] Ano de 2021
+                        [ 5 ] 1ºSemestre 2020
+                        [ 6 ] 2ºSemestre 2020
+                        [ 7 ] 1ºSemestre 2021
+                        [ 8 ] 2ºSemestre 2021\033[m''')
         esc = ""
 
         while esc != ("1", "2"):
-            esc = str(input('Digite a sua escolha(1 ou 2): '))
+            esc = str(input('Digite a sua escolha(1, 2, 3, 4, 5, 6, 7, 8): '))
 
             if esc == "1":
 
@@ -108,13 +192,12 @@ while x != ("cidade", "estado"):
                                       'last_available_death_rate': 'taxa de óbitos',
                                       'last_available_deaths': 'últimos óbitos',
                                       'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
-                print(colDT)
+                display(colDT)
                 break
 
             elif esc == "2":
-                dts = colCD.loc[colSP["date"] == "2021-05-10"]
+                dts = colCD.loc[colCD["date"] == "2021-07-11"]
 
-                dts = dts.drop("date", axis=1)
                 dts = dts.drop("state", axis=1)
                 dts = dts.drop("place_type", axis=1)
                 dts.rename(columns={'city': 'cidade', 'estimated_population': 'população',
@@ -122,7 +205,79 @@ while x != ("cidade", "estado"):
                                     'last_available_death_rate': 'taxa de óbitos',
                                     'last_available_deaths': 'últimos óbitos',
                                     'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
-                print(dts)
+                display(dts)
+                break
+
+            elif esc == "3":
+                Y = colCD[colCD["date"].between("2020-01-01", "2020-12-31")]
+                Y = Y.drop("state", axis=1)
+                Y = Y.drop("place_type", axis=1)
+                Y.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(Y)
+                break
+
+            elif esc == "4":
+                y = colCD[colCD["date"].between("2021-01-01", "2021-12-31")]
+                y = y.drop("state", axis=1)
+                y = y.drop("place_type", axis=1)
+                y.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(y)
+                break
+
+            elif esc == "5":
+                s = colCD[colCD["date"].between("2020-01-01", "2020-06-30")]
+                s = s.drop("state", axis=1)
+                s = s.drop("place_type", axis=1)
+                s.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(s)
+                break
+
+            elif esc == "6":
+                S = colCD[colCD["date"].between("2020-07-01", "2020-12-31")]
+                S = S.drop("state", axis=1)
+                S = S.drop("place_type", axis=1)
+                S.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                  'last_available_confirmed': 'últimos confirmados',
+                                  'last_available_death_rate': 'taxa de óbitos',
+                                  'last_available_deaths': 'últimos óbitos',
+                                  'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(S)
+                break
+
+            elif esc == "7":
+                s1 = colCD[colCD["date"].between("2021-01-01", "2020-06-30")]
+                s1 = s1.drop("state", axis=1)
+                s1 = s1.drop("place_type", axis=1)
+                s1.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                   'last_available_confirmed': 'últimos confirmados',
+                                   'last_available_death_rate': 'taxa de óbitos',
+                                   'last_available_deaths': 'últimos óbitos',
+                                   'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(s1)
+                break
+
+            elif esc == "8":
+                S1 = colCD[colCD["date"].between("2021-07-01", "2021-12-31")]
+                S1 = S1.drop("state", axis=1)
+                S1 = S1.drop("place_type", axis=1)
+                S1.rename(columns={'city': 'cidade', 'estimated_population': 'população',
+                                   'last_available_confirmed': 'últimos confirmados',
+                                   'last_available_death_rate': 'taxa de óbitos',
+                                   'last_available_deaths': 'últimos óbitos',
+                                   'new_confirmed': 'casos dia', 'new_deaths': 'óbitos dia'}, inplace=True)
+                print(S1)
                 break
 
     elif x == "x":
